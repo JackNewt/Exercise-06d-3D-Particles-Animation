9681fa22b2bb75efb02cae7c59ceff05a8bdb484
JackNewt <122556891+JackNewt@users.noreply.github.com>
2023-04-24 21:24:20 -0400

    Assignment Complete

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+onready var Explosion = load("res://Explosion/Explosion.tscn")
+onready var Explosions = get_node("/root/Game/Explosions")
 onready var Camera = $Pivot/Camera
 
 var gravity = -30
@@ -23,6 +25,9 @@ func _unhandled_input(event):
 func _physics_process(_delta):
 	$Pivot/Laser.transform.origin.z = 52.0
 	$Pivot/Laser.scale.y = 100.0
+	var explosion = Explosion.instance()
+	Explosions.add_child(explosion)
+	explosion.global_transform.origin = $Pivot/RayCast.get_collision_point()
 	$Pivot/Crosshair.transform.origin = orig_crosshair
 	$Pivot/Laser.hide()
 	if $Pivot/RayCast.is_colliding():
